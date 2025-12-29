588fca1b6ce084fff9828abbec481e65a274e41a
Dionis <228694295+dionis36@users.noreply.github.com>
2025-12-30 01:31:40 +0300

    feat: introduce new `ui` package with `sonner` toaster component and update `package-lock.json`.

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,3 +1,5 @@
 // Export components
 export * from "./components/GradientButton";
 export * from "./components/Logo";
+export { Toaster } from "./components/ui/sonner";
+export { toast } from "sonner";
