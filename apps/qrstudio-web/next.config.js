69986c86c3cd81cfbd8f8f24e7e262c0cafcb361
Dionis <228694295+dionis36@users.noreply.github.com>
2026-01-05 10:09:25 +0300

    fix: disable tracing and use static params

--- apps/qrstudio-web/next.config.js
+++ apps/qrstudio-web/next.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Monorepo support: Trace files from the workspace root
-    outputFileTracingRoot: path.join(__dirname, '../../'),
+    // Fix for Vercel "ENOENT: no such file or directory" trace error
+    outputFileTracing: false,
 
     // We trust our CI for type/lint checks, so we can skip them here to speed up deployment
     eslint: { ignoreDuringBuilds: true },
